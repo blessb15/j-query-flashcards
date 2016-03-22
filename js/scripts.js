6728ccde26e539987c5a25f07ff636c6fea76a85
Blake Bless and Ian Lesikar <ian@>
2016-03-22 15:01:28 -0700

    complete flashcards site w/ reset button

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#q1").click(function() {
+    $("#a1").show();
+  });
+  $("#q2").click(function() {
+    $("#a2").show();
+  });
+  $("#q3").click(function() {
+    $("#a3").show();
+  });
+  $("#q4").click(function() {
+    $("#a4").show();
+  });
+  $("#q5").click(function() {
+    $("#a5").show();
+  });
+  $("#red").click(function() {
+    $(".answer").hide();
+  });
+
+});
